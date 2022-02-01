21c499d780292fc2ac5ae191f09c82f89ba5aead
Shaan Khan <shaan.khan@stonybrook.edu>
2022-02-01 16:23:02 -0500

    Refactoring - Only data stored in src are jsons pointing to files in public.

--- src/Components/Screens/FindMe/findMe.js
+++ src/Components/Screens/FindMe/findMe.js
@@ -2,26 +2,18 @@
 import { LogoCard } from '../../index'
 import { Grid } from '@mui/material';
 
-import githubLogo from '../../../Images/Logos/github.png'
-import linkedinLogo from '../../../Images/Logos/linkedin.png'
-import soLogo from '../../../Images/Logos/so.jpeg'
-import twitterLogo from '../../../Images/Logos/twitter.png'
+import data from '../../../Data/FindMe.json';
 
 function FindMe() {
     return (
         <Grid container spacing={3}>
-            <Grid item xs={3}>
-                <LogoCard name={"Linked In"} image={linkedinLogo} link={"https://www.linkedin.com/in/shaan-khan-823a771b8/"} />
-            </Grid>
-            <Grid item xs={3}>
-                <LogoCard name={"GitHub"} image={githubLogo} link={"https://github.com/ContactShaanKhan"} />
-            </Grid>
-            <Grid item xs={3}>
-                <LogoCard name={"Twitter"} image={twitterLogo} link={"https://twitter.com/ctshaankhan"} />
-            </Grid>
-            <Grid item xs={3}>
-                <LogoCard name={"Stack Overflow"} image={soLogo} link={"https://stackoverflow.com/users/13132133/shaan-k"} />
-            </Grid>
+            {
+                data.map((item, index) => (
+                    <Grid item xs={3} key={index}>
+                        <LogoCard name={item.name} image={item.image} link={item.link} />
+                    </Grid>
+                ))
+            }
         </Grid>
     );
 }
