3fa70b3b2f04b2f1572ac15513b12391aabec7dc
Shaan Khan <contactshaankhan@gmail.com>
2022-02-04 11:19:28 -0500

    Adding more shadow photos and restructuring the shadow page.

--- src/Components/Screens/Shadow/shadow.js
+++ src/Components/Screens/Shadow/shadow.js
@@ -1,12 +1,11 @@
-import { useState } from 'react'
 import { Box, ImageList, ImageListItem } from '@mui/material'; 
 
 import itemData from '../../../Data/Shadow.json'
 
 function Shadow(props) {
     return (
-        <Box sx={{ width: "100%", height: "100%", padding:"5%", overflowY: 'scroll' }}>
-            <ImageList variant="woven" cols={10} gap={8}>
+        <Box sx={{ width: "90%", height: "100%", padding:"5%", overflowY: 'hidden' }}>
+            <ImageList variant="masonry" cols={3} gap={8}>
             {itemData.images.map((item) => (
                 <ImageListItem key={item.img}>
                 <img
