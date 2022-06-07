404dfe0939f7a7d2a2cd35b4203753cbe431f1d5
Shaan Khan <contactshaankhan@gmail.com>
2022-06-07 14:59:02 -0400

    Add TJ into the shadow page :).

--- src/Components/Screens/Shadow/shadow.js
+++ src/Components/Screens/Shadow/shadow.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { ImageList, ImageListItem } from '@mui/material';
+import { ImageList, ImageListItem, Typography } from '@mui/material';
 import { useMediaQuery } from 'react-responsive'
 import { GlobalStoreContext } from '../../../Store'
 import itemData from '../../../Data/Shadow.json'
@@ -9,22 +9,45 @@ function Shadow() {
 
     const isDesktopOrLaptop = useMediaQuery({ query: store.makeMediaQuery(1224) });
 
+
     // Reduce the number of columns if on the mobile view
     const numCols = (isDesktopOrLaptop) ? 4 : 2;
 
     return (
-        <ImageList variant="masonry" cols={numCols} gap={8}>
-            {itemData.images.map((item) => (
-                <ImageListItem key={item.img}>
-                    <img
-                        src={`${item.img}`}
-                        srcSet={`${item.img}`}
-                        alt={item.title}
-                        loading="lazy"
-                    />
-                </ImageListItem>
-            ))}
-        </ImageList>
+        <div style={{ width: "100%" }}>
+            <ImageList variant="masonry" cols={numCols} gap={8}>
+                {itemData.images.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img
+                            src={`${item.img}`}
+                            srcSet={`${item.img}`}
+                            alt={item.title}
+                            loading="lazy"
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+            <hr />
+            <div style={{
+                textAlign: "center",
+                marginTop: "50px",
+                marginBottom: "50px"
+            }}>
+                <Typography color={"white"} variant="h4">{`Other Cute Animals 🙂`}</Typography>
+            </div>
+            <ImageList variant="masonry" cols={numCols} gap={8}>
+                {itemData.otherImages.map((item) => (
+                    <ImageListItem key={item.img}>
+                        <img
+                            src={`${item.img}`}
+                            srcSet={`${item.img}`}
+                            alt={item.title}
+                            loading="lazy"
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
+        </div>
     );
 }
 
